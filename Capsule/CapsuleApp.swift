68b27a0ca3d2f2fed39a250757b47b19a7c1bc02
Barrışcan Aslan <i.aslan.bariscan@gmail.com>
2026-06-27 14:02:02 +0300

    Initial Commit

---
+++ Capsule/CapsuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapsuleApp.swift
+//  Capsule
+//
+//  Created by Barışcan Aslan on 27.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CapsuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
